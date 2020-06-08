6d082520a8404a3d14551e446eda49dfdb2c666c
apparatusjazz <maui.arcuri@gmail.com>
2020-06-07 22:29:06 -0400

    Delete button now appears on hover

--- src/components/point.js
+++ src/components/point.js
@@ -2,23 +2,33 @@ import React, { Component } from 'react'
 import '../css/point.css'
 
 class Point extends Component {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            hoverActive: false
+        }
+    }
     handleClick = () => {
         this.props.deletePoint(this.props.ms);
     }
+    handleMouseOver = () => {
+        this.setState({ hoverActive: true })
+    }
+    handleMouseLeave = () => {
+        this.setState({ hoverActive: false })
+    }
     render() {
-        let style = {};
-        let handleMouseOver = () => {
-            style = { display: "fixed" }
-        }
+        let displayStyle = this.state.hoverActive ? "inline-block" : "none";
+
         return (
             <div className="point"
-                onMouseOver={handleMouseOver}
+                onMouseOver={this.handleMouseOver}
+                onMouseLeave={this.handleMouseLeave}
                 style={{ left: this.props.left }} >
                 <button
                     className="dlt-btn"
                     onClick={this.handleClick}
-                    style={style}
+                    style={{ display: displayStyle }}
                 >
                     x
                 </button>
