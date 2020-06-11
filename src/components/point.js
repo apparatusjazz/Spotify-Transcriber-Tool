b031912c836d6c1e3a5d5c17a92e43855c9682e5
apparatusjazz <maui.arcuri@gmail.com>
2020-06-10 20:26:17 -0400

    Removed state because css can replace it

--- src/components/point.js
+++ src/components/point.js
@@ -1,27 +1,15 @@
 import React, { Component } from 'react'
 import '../css/point.css'
+import TimeStamp from './timestamp';
 
 class Point extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            hoverActive: false,
-        }
-    }
     handleClick = () => {
         this.props.deletePoint(this.props.ms);
-    }
-    handleMouseOver = () => {
-        this.setState({ hoverActive: true })
-    }
-    handleMouseLeave = () => {
-        this.setState({ hoverActive: false })
     }
     addLoopPoint = () => {
         this.props.addLoopPoint(this.props.ms);
     }
     render() {
-        let displayStyle = this.state.hoverActive ? "inline-block" : "none";
 
         return (
             <div className="point"
@@ -29,9 +17,10 @@ class Point extends Component {
                 onMouseLeave={this.handleMouseLeave}
                 style={{ left: this.props.left }}
             >
-                <div className="tooltip" style={{ display: displayStyle }}>
+                <div className="tooltip" >
+                    <TimeStamp timeStamp={this.props.ms} />
                     <button onClick={this.addLoopPoint}>
-                        s
+                        +
                     </button>
                     <button onClick={this.handleClick}>
                         x
