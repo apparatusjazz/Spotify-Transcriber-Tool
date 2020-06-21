069aa39199cafb94e15aadae44f1882826e2846e
apparatusjazz <maui.arcuri@gmail.com>
2020-06-20 23:16:39 -0400

    Added button png

--- src/components/controls.js
+++ src/components/controls.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react'
 import '../css/controls.css'
 import Play from './play';
-import loop from '../loop.png';
+import loop from '../assets/loop.png';
+import skipForward from '../assets/skip-forward.png';
+import skipBack from '../assets/skip-back.png';
+import beginning from '../assets/beginning.png';
 
 class Controls extends Component {
     handleBack() {
@@ -25,14 +28,32 @@ class Controls extends Component {
     render() {
         return (
             <div>
-                <button className="to-pnt" onClick={() => this.seekPosition(0)}>&lt;</button>
+                <img
+                    className="btn to-beginning"
+                    src={beginning} alt="beginning"
+                    onClick={() => this.seekPosition(0)}
+                />
+                <img
+                    className="btn skip"
+                    src={skipBack} alt="skipBack"
+                    onClick={() => this.seekPosition(0)}
+                />
                 <Play togglePlay={this.props.togglePlay} playing={this.props.playing} />
-                <button className="to-pnt" onClick={() => this.skipSeconds(1000)}>&gt;</button>
+                <img
+                    className="btn skip"
+                    src={skipForward} alt="skipFoward"
+                    onClick={() => this.skipSeconds(1000)}
+                />
+                <img
+                    className="btn"
+                    id="loop-img" src={loop}
+                    alt="loop"
+                    onClick={() => this.handleToggle()}
+                />
                 <div className="save-ctrls">
                     <button className="to-pnt" onClick={() => this.handleBack()}>&lt;</button>
                     <button className="save-pnt" onClick={() => this.handleClick()}>+</button>
                     <button className="to-pnt" onClick={() => this.handleNext()}>&gt;</button>
-                    <img style={{ position: 'absolute' }} id="loop-img" src={loop} alt="loop" onClick={() => this.handleToggle()} />
                 </div>
             </div>
 
