de29362ae2874559391ac97c18750a95e8ee8809
apparatusjazz <maui.arcuri@gmail.com>
2020-08-18 12:49:12 -0400

    Added Spotify track link

--- src/components/track-info.js
+++ src/components/track-info.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import '../css/track-info.css'
+import spotifyLogo from "../assets/Spotify_Icon_RGB_Green.png"
 
 class TrackInfo extends Component {
     render() {
@@ -7,6 +8,9 @@ class TrackInfo extends Component {
             <div className="track-info">
                 <img className="album-img" src={this.props.info.albumCover} alt="album cover" />
                 <div className="text">
+                    <a href={`https://open.spotify.com/track/${this.props.info.trackId}`} target="_blank">
+                        <img id="spotify-link" src={spotifyLogo} alt="Spotify Track"></img>
+                    </a>
                     <p id="track-name">{this.props.info.trackName}</p>
                     <p id="artist-name">{this.props.info.artist}</p>
                 </div>
